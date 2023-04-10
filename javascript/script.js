f4301b2889f2adf82d5c78d7c1dca8edfd5cff2b
Daniel Lam <Dann.lam@gmail.com>
2023-04-10 14:13:33 -0700

    Fleshed out base skelly for HTML

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+
+
+
+//GOAL: Dynamically add years 2024 to 2007 to get-car-year
+    //Make a function that dynamically adds car years to the
+    //id-get-car-year select
+    //creates an option with a value year starting from 2024
+    //descds to 2007
+    //Initialize years
+    //Handler for this select
+    //Once the year is selected, perform our queries.
+    //Once (carMake) populate the select with car make array from api
+    //We have to clear/reset the values of selections if som
+    //We could make a conditional check on the other fields if there is a match
