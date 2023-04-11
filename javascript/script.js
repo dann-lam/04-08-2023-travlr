61859b78adb5e46033287181d0d62a5409102a34
Daniel Lam <Dann.lam@gmail.com>
2023-04-11 11:19:36 -0700

    started intializing years on car years

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,97 @@
+let getCarYear = document.getElementById("get-car-year");
+
+let carInfo = document.getElementById("car-info");
 
 
-
-//GOAL: Dynamically add years 2024 to 2007 to get-car-year
-    //Make a function that dynamically adds car years to the
-    //id-get-car-year select
-    //creates an option with a value year starting from 2024
-    //descds to 2007
-    //Initialize years
     //Handler for this select
     //Once the year is selected, perform our queries.
     //Once (carMake) populate the select with car make array from api
     //We have to clear/reset the values of selections if som
     //We could make a conditional check on the other fields if there is a match
+
+
+//Sets the first option to each menu below the selected option to "please select"
+//create options of available years
+let init = () => {
+    initializeOptions();
+}
+
+let initializeYears = () => {
+
+    //start year to end year
+    let startYear = 1995;
+    let endYear = 2024
+
+    //initialize the "Start Here"
+
+
+
+    //count from start to end, create an option for each
+    //set that option's values
+    //append that option
+    for(let i = endYear; i > startYear - 1; i--){
+        let currYear = document.createElement("option");
+        currYear.value = `carYearVal-${i}`;
+        currYear.innerHTML = i;
+        currYear.id = `carYearBtn`;
+        getCarYear.appendChild(currYear)
+    }
+
+}
+let initializeOptions = (redrawMe) => {
+    //condition hit if  user selects an option above the chain.
+    //we reset the select options below the re-selected option
+    let siteInitialized = false;
+    if(redrawMe){
+
+    } else if (!siteInitialized){
+        //Should only be ran the first time the site is initialized.
+        for(let i = 0; i < carInfo.length; i++){
+            let startOption = document.createElement("option");
+            startOption.value = ""
+            startOption.innerHTML = "Select"
+            carInfo[i].appendChild(startOption);
+        }
+        siteInitialized = true;
+        initializeYears();
+    }
+
+}
+
+//Initializes website
+init();
+
+
+
+//REDRAW user options each time they select an option above.
+
+//create an option in the select area for every year.
+
+//when the user selects a year from the options, then
+
+//Make an API call to get the makes and put those in as an option for each make
+
+//After the user has selected a make
+
+//api call to get each car model, put those in as an option for each model
+
+//after user has selected a model
+
+//api call to get each car engine
+
+//after user has selected an engine
+
+//api call to get the average mpg.
+
+//As I select an option, it will reset options DOWN the DOM chain.
+//This way, if a user decides to change a year or something, it will reset the form.
+
+//If user hits "Get ID" Button, it will yell at the user to enter in information.
+
+
+document.querySelector('body').addEventListener('click', function(event) {
+    //This even fires on favorited buttons.
+if(event.target.className === "storedCity"){
+    getCityCoords(event, event.target.innerText)
+}
+})
