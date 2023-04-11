60e325904dd12c5fa60a65a2a6a0438772784f4d
Daniel Lam <Dann.lam@gmail.com>
2023-04-11 12:12:22 -0700

    added git stubs

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,20 @@
+// ROY-GET-ELEMENT
+// ROY-GET-ELEMENT
+// DANIEL-GET-ELEMENT
+// DANIEL-GET-ELEMENT
+// VICTORIA-GET-ELEMENT
+// VICTORIA-GET-ELEMENT
 
+// ROY-FUNCTIONS
+// ROY-FUNCTIONS
+// DANIEL-FUNCTIONS
+// DANIEL-FUNCTIONS
+// VICTORIA-FUNCTIONS
+// VICTORIA-FUNCTIONS
 
-
-//GOAL: Dynamically add years 2024 to 2007 to get-car-year
-    //Make a function that dynamically adds car years to the
-    //id-get-car-year select
-    //creates an option with a value year starting from 2024
-    //descds to 2007
-    //Initialize years
-    //Handler for this select
-    //Once the year is selected, perform our queries.
-    //Once (carMake) populate the select with car make array from api
-    //We have to clear/reset the values of selections if som
-    //We could make a conditional check on the other fields if there is a match
+// ROY-EVENT-LISTENERS
+// ROY-EVENT-LISTENERS
+// DANIEL-EVENT-LISTENERS
+// DANIEL-EVENT-LISTENERS
+// VICTORIA-EVENT-LISTENERS
+// VICTORIA-EVENT-LISTENERS
