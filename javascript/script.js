f012fd7d94e063994f0fdec357953b4d951c6be6
vjtolliver <vj.tolliver@gmail.com>
2023-04-12 16:03:13 -0700

    added effects to logo on refresh

--- javascript/script.js
+++ javascript/script.js
@@ -202,7 +202,28 @@ let initializeOptions = (redrawMe) => {
 //Initializes website
 init();
 // DANIEL-FUNCTIONS
-// VICTORIA-FUNCTIONS!
+// VICTORIA-FUNCTIONS
+var relativeEl = document.querySelector('#travlr-logo');
+relativeEl.style.transform = 'translateX(100px)';
+
+anime({
+    targets: '#travlr-logo',
+    translateX: {
+        value: '*=2.5', // 100px * 2.5 = '250px'
+        duration: 1000
+      },
+      width: {
+        value: '-=20px', // 28 - 20 = '8px'
+        duration: 1800,
+        easing: 'easeInOutSine'
+      },
+      rotate: {
+        value: '+=2turn', // 0 + 2 = '2turn'
+        duration: 1800,
+        easing: 'easeInOutSine'
+      },
+    direction: 'alternate',
+  });
 // VICTORIA-FUNCTIONS
 
 // const ctx = canvas.getContext("2d");
